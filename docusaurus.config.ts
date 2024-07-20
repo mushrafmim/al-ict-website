279bb64c153575fac360b8b02c915091c34140b8
Mohamed Mushraf <mushrafmim@gmail.com>
2024-07-20 08:11:48 +0530

    remove

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,11 +72,6 @@ const config: Config = {
         },
         { to: '/blog', label: 'Blog', position: 'left' },
         {
-          type: 'localeDropdown',
-          position: 'right',
-          to: '/ta'
-        },
-        {
           href: 'https://github.com/mushrafmim/al-ict-website',
           label: 'GitHub',
           position: 'right',
